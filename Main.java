376aa038e91377b5febe65cd1af0350a6ec7b2ea
jttagaza <jttagaza@up.edu.ph>
2018-11-20 13:20:31 +0800

    Added Main class

---
+++ Main.java
@@ -0,0 +1,9 @@
+import src.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Chat chat = new Chat("202.92.144.45", 80);
+    }
+}
