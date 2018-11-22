dbcf5083bc501e234a0eb6afe31b4742e71b1908
jttagaza <jttagaza@up.edu.ph>
2018-11-22 23:43:13 +0800

    Specified for ChatPacket

--- src/Chat.java
+++ src/Chat.java
@@ -1,34 +1,60 @@
 package src;
 
-import packet.PlayerProtos;
+import packet.PlayerProtos.*;
 import packet.TcpPacketProtos.TcpPacket.*;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-public class Chat {
+class Chat {
     private Socket socket;
 
-    public Chat(String addr, int port) {
-        try {
-            socket = new Socket(addr, port);
-            InputStream input = socket.getInputStream();
-            OutputStream output = socket.getOutputStream();
+    ChatPacket createChat(String message, Player player, String lobbyId){
+        ChatPacket.Builder chatBuilder = ChatPacket.newBuilder();
+        chatBuilder.setType(PacketType.CHAT);
+        chatBuilder.setMessage(message);
+        if(player != null)  chatBuilder.setPlayer(player);
+        if(lobbyId != null) chatBuilder.setLobbyId(lobbyId);
+        ChatPacket sentMsg = chatBuilder.build();
 
-            User user = new User();
-            ConnectPacket connect = ConnectPacket.newBuilder().setType(PacketType.CONNECT).setPlayer(user.getPlayer()).setLobbyId("AB5L").build();
-            socket.close();
-        }
-        catch(UnknownHostException unEx) {
-            System.out.println(unEx);
+        return sentMsg;
+    }
+
+    void send(ChatPacket sentMsg){
+        try {
+            OutputStream output = this.socket.getOutputStream();
+            DataOutputStream sender = new DataOutputStream(output);
+            sender.write(sentMsg.toByteArray());
         }
         catch(IOException err) {
-            System.out.println(err);
+            err.printStackTrace();
         }
+    }
+
+    ChatPacket receive(){
+        try {
+            InputStream input = this.socket.getInputStream();
+            DataInputStream receiver = new DataInputStream(input);
+            while(receiver.available() == 0){}
+            byte[] bytes = new byte[receiver.available()];
+            receiver.read(bytes);
+            ChatPacket receiveMsg = ChatPacket.parseFrom(bytes);
+            return receiveMsg;
+        }
+        catch(IOException err) {
+            err.printStackTrace();
+            return null;
+        }
+    }
+
+    public Chat(Socket socket) {
+        this.socket = socket;
     }
 }
