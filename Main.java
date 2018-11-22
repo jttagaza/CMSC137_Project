73eb645c16f5b9e199bd59f4c3cb738da682302b
jttagaza <jttagaza@up.edu.ph>
2018-11-22 23:42:19 +0800

    Change Chat class to Client class

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Chat chat = new Chat("202.92.144.45", 80);
+        Client client = new Client("202.92.144.45", 80);
     }
 }
