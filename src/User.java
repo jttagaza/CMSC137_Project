beeff472a9e4bfc2bf587323bab6f0e730721ee3
jttagaza <jttagaza@up.edu.ph>
2018-11-23 14:58:43 +0800

    Edited user class

--- src/User.java
+++ src/User.java
@@ -13,8 +13,23 @@ import java.net.UnknownHostException;
 class User {
     private Player player;
 
+    public final static int CREATE_LOBBY = 1;
+    public final static int CONNECT_LOBBY = 2;
+    public final static int EXIT = 0;
+
+    public int menu() {
+        System.out.println("[1] Create Lobby");
+        System.out.println("[2] Connect to a Lobby");
+        System.out.println("[0] Exit");
+        System.out.print("Enter choice: ");
+
+        Scanner str = new Scanner(System.in);
+        int choice = str.nextInt();
+        return choice;
+    }
+
     public User() {
-        System.out.println("Enter Name: ");
+        System.out.print("Enter Name: ");
         Scanner str = new Scanner(System.in);
         String name = str.nextLine();
         player = Player.newBuilder().setName(name).build();
