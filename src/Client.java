c67fffe99470b50f47b44f9bf0339f9350e64f30
jttagaza <jttagaza@up.edu.ph>
2018-11-23 10:02:22 +0800

    Included disconnect functionalities

--- src/Client.java
+++ src/Client.java
@@ -29,7 +29,7 @@ public class Client {
             System.out.println(receiveLobby);
 
             Connection connect = new Connection(socket);
-            ConnectPacket sentConnection = connect.createConnection(user.getPlayer(), receiveLobby.getLobbyId(), ConnectPacket.Update.NEW);
+            ConnectPacket sentConnection = connect.createConnection(user.getPlayer(), receiveLobby.getLobbyId());
             connect.send(sentConnection);
             ConnectPacket receiveConnection = connect.receive();
             System.out.println(receiveConnection);
@@ -39,6 +39,14 @@ public class Client {
             chat.send(sentMsg);
             ChatPacket receiveMsg = chat.receive();
             System.out.println(receiveMsg);
+
+            Disconnection disconnect = new Disconnection(socket);
+            DisconnectPacket sentDisconnection = disconnect.removeConnection(user.getPlayer());
+            disconnect.send(sentDisconnection);
+            // Err. Receive function on Disconnect just waiting for empty Response
+            // DisconnectPacket receiveDisconnection = disconnect.receive();
+            // System.out.println(receiveDisconnection);
+
             this.socket.close();
         }
         catch(UnknownHostException unEx) {
